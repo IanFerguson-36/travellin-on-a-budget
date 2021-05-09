a9c9019d10fb3219b381a6193897257e849e2f42
Ian F <ian.ferg36@gmail.com>
2021-05-09 13:52:10 -0600

    changed port number, and wanted to push one more time with a correct gitignore

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 30;
+const PORT = 3026;
 
 const app = express();
 
